0a6c5ac0f03a3dde87edbb3997e26de3d6282c6c
Carlito Algarin <carlito1113@yahoo.com>
2021-09-05 20:48:03 -0400

    commit for file change

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,38 +1,76 @@
 import React, { Fragment, useState } from 'react';
+import axios from 'axios';
 
 export default function Register() {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     password: '',
-    password2: ''
+    password2: '',
   });
 
   const { name, email, password, password2 } = formData;
 
-  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value, [e.target.email]: e.target.value, [e.target.password]: e.target.value, [e.target.password2]: e.target.value });
+  const onChange = e =>
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
       console.log('Passwords do not match');
     } else {
-      console.log(formData);
+      const newUser = {
+        name,
+        email,
+        password,
+      };
+
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+
+        const body = JSON.stringify(newUser);
+
+        const res = await axios.post('/api/users', body, config);
+        console.log(res.data);
+      } catch (err) {
+        console.error(err.response.data);
+      }
     }
-  }
-  
+  };
+
   return (
     <Fragment>
       <h1 className="large text-primary">Sign Up</h1>
       <p className="lead">
         <i className="fas fa-user"></i> Create Your Account
       </p>
-      <form className="form" onSubmit={e => onSubmit(e)} >
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
-          <input type="text" placeholder="Name" name='name' value={name} onChange={e => onChange(e)} required />
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={e => onChange(e)}
+            required
+          />
         </div>
         <div className="form-group">
-          <input type="email" placeholder="Email Address" name='email' value={email} onChange={e => onChange(e)} required />
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
 
           <small className="form-text">
             This site uses Gravatar, so if you want a profile image, use a
@@ -40,10 +78,26 @@ export default function Register() {
           </small>
         </div>
         <div className="form-group">
-          <input type="password" placeholder="Password" minlength="6"  name='password' value={password} onChange={e => onChange(e)} required />
+          <input
+            type="password"
+            placeholder="Password"
+            minLength="6"
+            name="password"
+            value={password}
+            onChange={e => onChange(e)}
+            required
+          />
         </div>
         <div className="form-group">
-          <input type="password" placeholder="Confirm Password" minlength="6" name='password2' value={password2} onChange={e => onChange(e)} required />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            minLength="6"
+            name="password2"
+            value={password2}
+            onChange={e => onChange(e)}
+            required
+          />
         </div>
         <input type="submit" value="Register" className="btn btn-primary" />
       </form>
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,3 +1,96 @@
+// const express = require('express');
+// const router = express.Router();
+// const gravatar = require('gravatar');
+// const bcrypt = require('bcryptjs');
+// const jwt = require('jsonwebtoken');
+// const config = require('config');
+// const { check, validationResult } = require('express-validator');
+
+// const User = require('../../models/User');
+
+// // @route   POST api/users
+// // @desc    Register User
+// // @access  Public
+// router.post(
+//   '/',
+//   [
+//     check('name', 'Name is required').notEmpty(),
+//     check('email', 'Please include a valid email').isEmail(),
+//     check(
+//       'password',
+//       'Please enter a password with 6 or more characters'
+//     ).isLength({ min: 6 }),
+//   ],
+//   async (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       return res.status(400).json({ errors: errors.array() });
+//     }
+
+//     const { name, email, password } = req.body;
+
+//     try {
+//       /////////////////
+//       // Check for user
+//       let user = await User.findOne({ email });
+
+//       if (user) {
+//         return res
+//           .status(400)
+//           .json({ errors: [{ msg: 'User already exists' }] });
+//       }
+
+//       /////////////////////////
+//       // Get the users gravatar
+//       const avatar = gravatar.url(email, {
+//         s: '200',
+//         r: 'pg',
+//         d: 'mm',
+//       });
+
+//       user = new User({
+//         name,
+//         email,
+//         avatar,
+//         password,
+//       });
+
+//       ///////////////////
+//       // Encrypt password
+//       const salt = await bcrypt.genSalt(10);
+
+//       user.password = await bcrypt.hash(password, salt);
+
+//       await user.save();
+
+//       //////////////////////
+//       // get the payload
+//       const payload = {
+//         user: {
+//           id: user.id,
+//         },
+//       };
+
+//       /////////////////
+//       // sign the token
+//       jwt.sign(
+//         payload,
+//         config.get('jwtSecret'),
+//         { expiresIn: 3600 },
+//         (err, token) => {
+//           if (err) throw err;
+//           res.json({ token });
+//         }
+//       );
+//     } catch (err) {
+//       console.log(err.message);
+//       res.status(500).send('Server error');
+//     }
+//   }
+// );
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
@@ -5,22 +98,21 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
+const normalize = require('normalize-url');
 
 const User = require('../../models/User');
 
-// @route   POST api/users
-// @desc    Register User
-// @access  Public
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
 router.post(
   '/',
-  [
-    check('name', 'Name is required').not().isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
-    check(
-      'password',
-      'Please enter a password with 6 or more characters'
-    ).isLength({ min: 6 }),
-  ],
+  check('name', 'Name is required').notEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check(
+    'password',
+    'Please enter a password with 6 or more characters'
+  ).isLength({ min: 6 }),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -30,8 +122,6 @@ router.post(
     const { name, email, password } = req.body;
 
     try {
-      /////////////////
-      // Check for user
       let user = await User.findOne({ email });
 
       if (user) {
@@ -40,50 +130,45 @@ router.post(
           .json({ errors: [{ msg: 'User already exists' }] });
       }
 
-      /////////////////////////
-      // Get the users gravatar
-      const avatar = gravatar.url(email, {
-        s: '200',
-        r: 'pg',
-        d: 'mm',
-      });
+      const avatar = normalize(
+        gravatar.url(email, {
+          s: '200',
+          r: 'pg',
+          d: 'mm'
+        }),
+        { forceHttps: true }
+      );
 
       user = new User({
         name,
         email,
         avatar,
-        password,
+        password
       });
 
-      ///////////////////
-      // Encrypt password
       const salt = await bcrypt.genSalt(10);
 
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
 
-      //////////////////////
-      // get the payload
       const payload = {
         user: {
-          id: user.id,
-        },
+          id: user.id
+        }
       };
 
-      /////////////////
-      // sign the token
       jwt.sign(
         payload,
         config.get('jwtSecret'),
-        { expiresIn: 3600 },
+        { expiresIn: '5 days' },
         (err, token) => {
           if (err) throw err;
           res.json({ token });
         }
       );
     } catch (err) {
-      console.log(err.message);
+      console.error(err.message);
       res.status(500).send('Server error');
     }
   }

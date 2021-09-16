23d4fc112fa37ecd568fc98bfca6cefed528c10f
Carlito Algarin <carlito1113@yahoo.com>
2021-09-16 01:37:47 -0400

    created the profile about page and the profile top section

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
@@ -13,7 +15,7 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getProfileById]);
+  }, [getProfileById, match.params.id]);
 
   return <>
     {profile === null || loading ? <Spinner /> : <>
@@ -23,6 +25,10 @@ const Profile = ({
       {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to="/edit-profile" className='btn btn-dark' >
         Edit Profile
       </Link>)}
+      <div className="profile-grid my-1">
+        <ProfileTop profile={profile} />
+        <ProfileAbout profile={profile} />
+      </div>
     </>}
   </>;
 };

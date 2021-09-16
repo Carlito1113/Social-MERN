516d7ee02fe989b9cfa080d32c80ec6ada16e3c0
Carlito Algarin <carlito1113@yahoo.com>
2021-09-16 00:24:58 -0400

    created profile component

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -10,6 +10,7 @@ import AddExperience from '../profile-forms/AddExperience';
 import AddEducation from '../profile-forms/AddEducation';
 import NotFound from '../layout/NotFound';
 import Profiles from '../profiles/Profiles';
+import Profile from '../profiles/Profile';
 import PrivateRoute from '../routing/PrivateRoute';
 
 const Routes = () => {
@@ -20,6 +21,7 @@ const Routes = () => {
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/profiles" component={Profiles} />
+        <Route exact path="/profile/:id" component={Profile} />
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
         <PrivateRoute exact path="/create-profile" component={CreateProfile} />
         <PrivateRoute exact path="/edit-profile" component={EditProfile} />
